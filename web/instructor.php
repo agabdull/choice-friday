014754e187aecb31222cc475470bb179e3394e61
Ali Abdulla <aligabdulla1@gmail.com>
2020-08-26 19:56:30 -0400

    form clears on successful submits.  also, LOGOUT has become log out

--- web/instructor.php
+++ web/instructor.php
@@ -19,7 +19,7 @@ if(isset($_SESSION['userEmail']) && isset($_SESSION['userGrade']) && isset($_SES
 $submitMessage = "";
     
 function getInputValue($name){
-    if(isset($_POST[$name])){
+    if(isset($_POST[$name]) && $submitMessage != "SUCCESS"){ // clear form on successes
         echo $_POST[$name];
     }
 }
@@ -65,7 +65,7 @@ if(isset($_POST['addButton'])){
     <script src="assets/js/scripts.js"></script>
 </head>
 <body>
-    <button id="logoutButton"class="button" onclick="logout()">LOG OUT</button>
+    <button id="logoutButton"class="button" onclick="logout()">Log out</button>
 
     <!--<p>Hello, <?php echo $userFirstName?>. You are an instructor, and your email address is <?php echo $userEmail?> </p> -->
 
