93ca9f2cd391c5a7370b6fbb301adab6b7f8b9d1
Ali Abdulla <aligabdulla1@gmail.com>
2020-08-26 15:36:32 -0400

    It worked!  Uncommenting out instructor.php redirect now

--- web/index.php
+++ web/index.php
@@ -17,7 +17,7 @@ if(isset($_SESSION['userEmail'])){
     $userFirstName = $_SESSION['userFirstName'];
 
     if ($userGrade == 0){
-       // header("Location: instructor.php");
+       header("Location: instructor.php");
     }
 
 } else {
