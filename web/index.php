a25f77f9811f58ccf1aa8f2be0e5d8021e5c892d
Ali Abdulla <aligabdulla1@gmail.com>
2020-08-29 10:17:21 -0400

    using implosion of array to string in order to use the choiceArr.  problem not fixed as of yet

--- web/index.php
+++ web/index.php
@@ -67,7 +67,9 @@ if(isset($_POST['chooseButton'])){
 
 
     // update userchoices
-    $query = $pdo->query("UPDATE userchoices SET choices = ARRAY '$choiceArr' WHERE email='$userEmail'"); 
+    //$query = $pdo->query("UPDATE userchoices SET choices = ARRAY '$choiceArr' WHERE email='$userEmail'");  nope
+    $choiceArrImploded = implode(",",$choiceArr);
+    $query = $pdo->query("UPDATE userchoices SET choices = '$choiceArrImploded' WHERE email='$userEmail'"); 
     if ($query){
         echo "Query to update userchoices successful";
     } else {
